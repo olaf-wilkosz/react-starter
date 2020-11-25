c584c7136e7dab9242ccae16746a5ee6f51b4e09
Olaf Wilkosz <olaf.wilkosz@gmail.com>
2020-11-25 22:48:51 +0100

    Testing JSX syntax

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,10 +3,10 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <main className="app-content">
         <h1>My first React app</h1>
         <h2>Hello world!</h2>
-      </div>
+      </main>
     )
   }
 }
