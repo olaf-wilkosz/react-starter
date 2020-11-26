c9fb143e3b36a12220e3eec4d8b5d88797d63715
Olaf Wilkosz <olaf.wilkosz@gmail.com>
2020-11-26 22:44:42 +0100

    Add function addCard and Creator component to Column, add Icon component to columns

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
+import Card from '../Card/Card';
+import { settings } from '../../data/dataStore';
+import Creator from '../Creator/Creator';
+import Icon from '../Icon';
 
 class Column extends React.Component {
+  state = {
+    cards: this.props.cards || [],
+  }
   static propTypes = {
     title: PropTypes.string,
+  }
+
+  addCard(title) {
+    this.setState(state => (
+      {
+        cards: [
+          ...state.cards,
+          {
+            key: state.cards.length ? state.cards[state.cards.length - 1].key + 1 : 0,
+            title,
+            cards: []
+          }
+        ]
+      }
+    ));
   }
 
   render() {
     return (
       <section className={styles.component}>
-        <h3 className={styles.title}>{this.props.title}</h3>
+        <h3 className={styles.title}><span className={styles.icon}><Icon name={this.props.icon}/></span>{this.props.title}</h3>
+        <div>
+          {this.state.cards.map(({ key, ...cardProps }) => (
+            <Card key={key} {...cardProps} />
+          ))}
+        </div>
+        <div>
+          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
+        </div>
       </section>
     )
   }
