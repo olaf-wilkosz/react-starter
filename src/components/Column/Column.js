539b12521286a7463016a7001218185aca279809
Olaf Wilkosz <olaf.wilkosz@gmail.com>
2020-11-26 20:43:37 +0100

    Fix props in Column

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -10,7 +10,7 @@ class Column extends React.Component {
   render() {
     return (
       <section className={styles.component}>
-        <h3 className={styles.title}>{this.props.columns}</h3>
+        <h3 className={styles.title}>{this.props.columns.title}</h3>
       </section>
     )
   }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,18 +3,18 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero';
 import Column from '../Column/Column';
 import PropTypes from 'prop-types';
+import { settings } from '../../data/dataStore';
 
 class List extends React.Component {
   static propTypes = {
     title: PropTypes.node.isRequired,
     image: PropTypes.string.isRequired,
-    children: PropTypes.node,
-    columns: PropTypes.arrayOf(PropTypes.object).isRequired,
+    description: PropTypes.node,
+    columns: PropTypes.array,
   }
 
   static defaultProps = {
-    children: <p>I can do all the things!!!</p>,
-    columns: ['Column1', 'Column2', 'Column3'],
+    description: settings.defaultListDescription,
   }
 
   render() {
@@ -22,7 +22,7 @@ class List extends React.Component {
       <section className={styles.component}>
         <Hero titleText={this.props.title} imageAddress={this.props.image} />
         <div className={styles.description}>
-          {this.props.children}
+          {this.props.description}
         </div>
         <div className={styles.columns}>
           <Column columns={this.props.columns[0]} />
