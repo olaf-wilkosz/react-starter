0e0f4fed5e49e20e7eaa9993ce3d1f7475dd903f
Olaf Wilkosz <olaf.wilkosz@gmail.com>
2020-11-26 21:24:07 +0100

    Add ReactHtmlParser to title in Hero and description in List

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,6 +4,7 @@ import Hero from '../Hero/Hero';
 import Column from '../Column/Column';
 import PropTypes from 'prop-types';
 import { settings } from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
 
 class List extends React.Component {
   static propTypes = {
@@ -22,7 +23,7 @@ class List extends React.Component {
       <section className={styles.component}>
         <Hero titleText={this.props.title} imageAddress={this.props.image} />
         <div className={styles.description}>
-          {this.props.description}
+          {ReactHtmlParser(this.props.description)}
         </div>
         <div className={styles.columns}>
           <Column columns={this.props.columns[0]} />
