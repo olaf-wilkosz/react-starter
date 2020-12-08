63b839d8816c4e2f09730eb925be4fdfb3319b34
Olaf Wilkosz <olaf.wilkosz@gmail.com>
2020-12-08 21:30:03 +0100

    Add new Button variant for info

--- src/components/Creator/Creator.js
+++ src/components/Creator/Creator.js
@@ -44,6 +44,11 @@ class Creator extends React.Component {
     }
   }
 
+  handleInfo = () => {
+    if (window.alert('Hit OK to save new title or cancel to clear')) {
+    }
+  }
+
   render() {
     return (
       <div className={styles.component}>
@@ -56,6 +61,7 @@ class Creator extends React.Component {
         <div className={styles.buttons + (this.state.visibleButtons ? ' ' + styles.buttonsShown : '')}>
           <Button onClick={this.handleOK}>OK</Button>
           <Button onClick={this.handleCancel} variant='danger'>cancel</Button>
+          <Button onClick={this.handleInfo} variant='info'>Info</Button>
         </div>
       </div>
     );
