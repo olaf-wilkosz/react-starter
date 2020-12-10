2bb4a77d5b02a52d2aac0255a2bcc3718ca47525
Olaf Wilkosz <olaf.wilkosz@gmail.com>
2020-12-11 00:24:50 +0100

    Add AppContainer.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,46 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import List from '../List/List';
-import Creator from '../Creator/Creator';
-import { pageContents, listData } from '../../data/dataStore';
-import { settings } from '../../data/dataStore';
+// import List from '../List/List';
 import styles from './App.scss';
 
 class App extends React.Component {
-  state = {
-    listData: this.props.listData || [],
-  }
   static propTypes = {
-    listData: PropTypes.node,
-  }
-
-  addList(title) {
-    this.setState(state => (
-      {
-        listData: [
-          ...state.listData,
-          {
-            key: state.listData.length ? state.listData[state.listData.length - 1].key + 1 : 0,
-            title,
-            image: 'https://images.pexels.com/photos/230744/pexels-photo-230744.jpeg',
-            description: [],
-            columns: [],
-          },
-        ],
-      }
-    ));
+    title: PropTypes.node,
+    subtitle: PropTypes.node,
   }
 
   render() {
+    const { title, subtitle } = this.props;
     return (
       <main className={styles.component}>
-        <h1 className={styles.title}>{pageContents.title}</h1>
-        <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
+        <h1 className={styles.title}>{title}</h1>
+        <h2 className={styles.subtitle}>{subtitle}</h2>
+        {/*
         <List {...listData} />
-        {this.state.listData.map(({ key, ...listDataProps }) => (
-          <List key={key} {...listDataProps} />
-        ))}
-        <Creator text={settings.listCreatorText} image="{settings.defaultImage}" action={title => this.addList(title)} />
+        */}
       </main>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import App from './components/App/App';
+import App from './components/App/AppContainer';
 import './styles/normalize.scss';
 import './styles/global.scss';
 
