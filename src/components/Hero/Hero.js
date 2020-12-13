26b6731219d821ab33f9c4019d012ea32add44be
Olaf Wilkosz <olaf.wilkosz@gmail.com>
2020-12-13 17:44:52 +0100

    Fix props for Hero image

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,13 +6,13 @@ import styles from './Hero.scss';
 const Hero = props => (
   <header className={styles.component}>
     <h2 className={styles.title}>{ReactHtmlParser(props.titleText)}</h2>
-    <img className={styles.image} src={props.imageAddress} />
+    <img className={styles.image} src={props.image} />
   </header>
 );
 
 Hero.propTypes = {
   titleText: PropTypes.node.isRequired,
-  imageAddress: PropTypes.string,
+  image: PropTypes.string,
 };
 
 export default Hero;
