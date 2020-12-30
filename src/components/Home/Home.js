6077b910f9437ec0eadd49a233af07dea02ebe56
Olaf Wilkosz <olaf.wilkosz@gmail.com>
2020-12-30 22:48:39 +0100

    Modify Home component to use ListLink instead of List

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import List from '../List/ListContainer';
-import Search from '../Search/SearchContainer';
+import ListLink from '../ListLink/ListLink';
 import styles from './Home.scss';
 
 class Home extends React.Component {
@@ -17,9 +16,8 @@ class Home extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
-        <Search />
         {lists.map(listData => (
-          <List key={listData.id} {...listData} />
+          <ListLink key={listData.id} {...listData} />
         ))}
       </main>
     );
